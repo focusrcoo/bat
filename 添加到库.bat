7c56c830a0be6de3cb03060bf2a20bd164c8b002
focusrcoo <166787713+focusrcoo@users.noreply.github.com>
2024-04-12 22:01:21 +0800

    Add files via upload

---
+++ 添加到库.bat
@@ -0,0 +1,12 @@
+@echo off
+title 添加到番号库
+mode con: cols=35 lines=10
+echo.
+set /p fanhao=请输入番号:
+
+find /i "%fanhao%" Summary.txt>nul&& echo 已存在||(echo %fanhao%>>Summary.txt
+ echo 已增加)
+
+ping 127.0.0.1 -n 4 >nul
+
+%0
