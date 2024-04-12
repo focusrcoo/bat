996a797afde6ec7fc32cf38c5dd030c2c3010b3e
focusrcoo <166787713+focusrcoo@users.noreply.github.com>
2024-04-12 22:03:42 +0800

    Add files via upload

--- 添加到库.bat
+++ 添加到库.bat
@@ -1,8 +1,8 @@
 @echo off
-title 添加到番号库
+title 添加到库
 mode con: cols=35 lines=10
 echo.
-set /p fanhao=请输入番号:
+set /p fanhao=请输入号码:
 
 find /i "%fanhao%" Summary.txt>nul&& echo 已存在||(echo %fanhao%>>Summary.txt
  echo 已增加)
